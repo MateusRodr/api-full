be0a3eac37204c600db160c5b9eea40d8f901415
MateusRodr <mr5508113@gmail.com>
2025-10-17 13:50:46 -0300

    injection dependency with the prisma client

--- src/repository/prisma-task-repository.ts
+++ src/repository/prisma-task-repository.ts
@@ -3,33 +3,34 @@ import { PrismaClient } from "@prisma/client";
 import { Task } from "../prisma/entity/task.entity";
 import { PrismaTaskMapper } from "../prisma/mappers/prisma-task-mapper";
 
-const prisma = new PrismaClient();
+
 
 @injectable()
 export class PrismaTaskRepository {
+  constructor(private prisma: PrismaClient) {}
   async create(task: Task): Promise<Task> {
     const data = PrismaTaskMapper.toPrisma(task);
-    const created = await prisma.task.create({ data });
+    const created = await this.prisma.task.create({ data });
     return PrismaTaskMapper.toDomain(created);
   }
 
   async findAll(): Promise<Task[]> {
-    const tasks = await prisma.task.findMany();
+    const tasks = await this.prisma.task.findMany();
     return tasks.map(PrismaTaskMapper.toDomain);
   }
 
   async findById(id: number): Promise<Task | null> {
-    const task = await prisma.task.findUnique({ where: { id } });
+    const task = await this.prisma.task.findUnique({ where: { id } });
     return task ? PrismaTaskMapper.toDomain(task) : null;
   }
 
   async findByTitle(title: string): Promise<Task | null> {
-    const task = await prisma.task.findFirst({ where: { title: title } });
+    const task = await this.prisma.task.findFirst({ where: { title: title } });
     return task ? PrismaTaskMapper.toDomain(task) : null;
   }
 
   async update(id: number, task: Task): Promise<Task> {
-    const updated = await prisma.task.update({
+    const updated = await this.prisma.task.update({
       where: { id },
       data: PrismaTaskMapper.toPrisma(task),
     });
@@ -37,6 +38,6 @@ export class PrismaTaskRepository {
   }
 
   async delete(id: number): Promise<void> {
-    await prisma.task.delete({ where: { id } });
+    await this.prisma.task.delete({ where: { id } });
   }
 }
--- src/shared /container.ts
+++ src/shared /container.ts
@@ -1,6 +1,8 @@
 import { container } from "tsyringe";
 import { PrismaTaskRepository } from "../repository/prisma-task-repository";
 import { TaskService } from "../services/taskService";
+import { PrismaClient } from "@prisma/client";
 
+container.registerInstance(PrismaClient, new PrismaClient());
 container.registerSingleton(PrismaTaskRepository);
 container.registerSingleton(TaskService);
